83550c2e9aad6e4b0e7b73e2619596603a9c519e
Srvictorchinea <sr.victorchinea@gmail.com>
2024-01-27 23:45:56 +0000

    [ADD] offers banner

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Offers.css' 
-
+import exclucive_image from '../Assets/exclusive_image.png'
 const Offers = () => {
   return (
     <div className='offers'>
-
+        <div className="offers-left">
+            <h2>Exclusive</h2>
+            <h2>Offers for you</h2>
+            <p>ONLY BEST SELLERS PRODUCTS</p>
+            <button>Check Now</button>
+        </div>
+        <div className="offers-right">
+            <img src={exclucive_image} alt="" />
+        </div>
     </div>
   )
 }
