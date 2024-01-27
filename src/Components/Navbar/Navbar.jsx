eec6f10f2e19cfcb551ccadd4fa23367e884d50d
Srvictorchinea <sr.victorchinea@gmail.com>
2024-01-27 14:23:29 +0000

    [ADD] navbar component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState }  from "react";
+import "./Navbar.css";
+import logo from "../Assets/logo.png";
+import cart_icon from "../Assets/cart_icon.png";
 const Navbar = () => {
-  return (
-    <div className='navbar'>Navbar</div>
-  )
-}
 
-export default Navbar
+    const [menu,setmenu] = useState("shop");
+
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li onMouseOver={() => {setmenu("shop")}}>Shop{menu=="shop"?<hr />:<></>}</li>
+        <li onMouseOver={() => {setmenu("mens")}}>Women{menu=="mens"?<hr />:<></>}</li>
+        <li onMouseOver={() => {setmenu("womens")}}>Men{menu=="womens"?<hr />:<></>}</li>
+        <li onMouseOver={() => {setmenu("kids")}}>Kids{menu=="kids"?<hr />:<></>}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
