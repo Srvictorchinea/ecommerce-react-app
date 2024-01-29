9872b9918a60e7063a9bbc48d1459d7d42f34e74
Srvictorchinea <sr.victorchinea@gmail.com>
2024-01-29 15:16:11 +0000

    [ADD] products links

--- src/Components/Breadcrums/Breadcrum.jsx
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -6,6 +6,9 @@ const Breadcrum = (props) => {
   return (
     <div className='breadcrum'>
         HOME <img src={arrow_icon} alt="" />
+        SHOP <img src={arrow_icon} alt="" />
+        {product.category} <img src={arrow_icon} alt="" />
+        {product.name} <img src={arrow_icon} alt="" />
     </div>
   )
 }
