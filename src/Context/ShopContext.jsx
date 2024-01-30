d5e71b687d0eb51be08cb1080bbf85f162dbb647
Srvictorchinea <sr.victorchinea@gmail.com>
2024-01-30 22:31:35 +0000

    [ADD] cart

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -16,13 +16,36 @@ const ShopContextProvider = (props) => {
 
     const addToCart   = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]:prev[itemId]+1}))
+        console.log(cartItems)
     }
 
-    const removeFromCart   = (itemId) => {
+    const removeFromCart = (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]:prev[itemId]-1}))
     }
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if(cartItems[item]>0) {   
+                let itemInfo = all_product.find((product) => product.id === Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item]
+            }
+        }
+        return totalAmount;
+    }
+
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+
+        for (const item in cartItems) {
+            if (cartItems[item]>0){
+                totalItem += cartItems[item]
+            }
+        }
+        return totalItem
+    }
     
-    const contextValue = {all_product, cartItems, addToCart, removeFromCart}
+    const contextValue = { getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart, getTotalCartItems }
 
     return (
         <ShopContext.Provider value={contextValue}>
